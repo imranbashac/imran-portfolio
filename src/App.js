87ca102be1cbfcbf9bfc9fe0a4ae73540ddb29a8
Imran Basha <cmimranbasha@gmail.com>
2025-08-03 20:21:25 +0300

    Update project with latest changes and tailwind config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-gray-900 text-white flex items-center justify-center text-4xl font-bold">
+      Imran's Portfolio 🚀
     </div>
   );
 }
